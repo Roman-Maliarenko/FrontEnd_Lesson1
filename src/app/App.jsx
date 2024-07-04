71db9a5325b3e274d0742be2b2ebe28c103071e3
Roman-Maliarenko <romamalarenko1@gmail.com>
2024-07-04 23:17:40 +0300

    add me

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,7 +5,14 @@ function App() {
   return (
     <div className="App">
       <h1>Hello React 🚀</h1>
+      <h>======================</h>
+      <h1>Hello my name is Roman</h1>
+      <h2>I`m 19 years old</h2>
+      <h3>My Hobby:</h3>
+      <h2>Volleyball</h2>
+      <img src="https://www.volleyball-verband.de/images/redaktion/News/2024/Halle/Natio-Maenner/VNL/VNL-M_GER-JPN_1_volleyballworld_2580b_c_735x413.jpg" alt="" />
     </div>
+  
   );
 }
 
---
+++ src/lessons/lesson_01/Lesson01.jsx
@@ -0,0 +1,41 @@
+import './lesson01.css'
+
+export default function Lesson01() {
+
+    //console.dir(document);
+
+    const heading = 'Lesson 01'
+    const classContainer = 'lesson1-container'
+
+    function formatName(user){
+        return `Name: ${user.name}, lastname ${user.lastname}`
+    };
+
+    const user = {
+        name: 'John',
+        lastname: 'Doe'
+    };
+
+    const isLogin = true
+
+
+    return (
+    <div className = {classContainer}>
+        <h4>{heading}</h4>
+        <h5>Особености компонента:</h5>
+        <ul>
+            <li>Имеет расширение JSX</li>
+            <li>Называеться с большой буквы</li>
+            <li>Компонент это функция</li>
+            <li>Возвращяет верстку</li>
+            <li>Использует import / export</li>
+            <li>Не отобразится на странице, если не импортирован</li>
+        </ul>
+        <p>Пример работы с динамическими данными:</p>
+        <span>{formatName(user)}</span>
+        <h5>Пример условного отобрадения с тернарным  оператором:</h5>
+        <span>{isLogin ? 'в данный момент' : 'не'} авторизован</span>
+        
+    </div>
+    );
+}
